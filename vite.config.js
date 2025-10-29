4d8ef89af6e374053cda6c2891d9328d61f91106
Aditya Hawaari <adityahawaari550@gmail.com>
2025-10-29 13:09:35 +0700

    Setup GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/keuangan-app-vite/',   // <— penting untuk GitHub Pages
   plugins: [react()],
 })
